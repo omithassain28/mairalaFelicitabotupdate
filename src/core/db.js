1e636b6699c6d38a647749a478132c97b6f83db1
omithassain28 <115416573+omithassain28@users.noreply.github.com>
2024-02-28 14:10:57 +0600

    Update db.js

--- src/core/db.js
+++ src/core/db.js
@@ -1,9 +1,7 @@
-// ... const { Database } = require('quickmongo');
-const mongoose = require('mongoose');
-
+const { Database } = require('quickmongo');
 const db = new Database('mongodb+srv://Rakibxox:omit968965@cluster0.lbvtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 db.connect().then(() => console.log('[ MONGO DB ] Connected to Mongo Database!'));
 
-mongoose.set('strictQuery', false);
+quickmongo.set('strictQuery', false);
 
 module.exports = db;
