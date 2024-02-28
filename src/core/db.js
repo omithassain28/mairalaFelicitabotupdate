07af08b3c189f716ae7f941b52bd8269c2de5c8f
omithassain28 <115416573+omithassain28@users.noreply.github.com>
2024-02-28 17:20:46 +0600

    Delete src directory

--- src/core/db.js
+++
@@ -1,6 +0,0 @@
-const { Database } = require('quickmongo');
-
-const db = new Database('mongodb+srv://Rakibxox:omit968965@cluster0.lbvtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-db.connect().then(() => console.log('[ MONGO DB ] Connected to Mongo Database!'));
-
-module.exports = db;
