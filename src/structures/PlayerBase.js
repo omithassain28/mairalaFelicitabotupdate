16b93ce451ecb1a846969792340559083f7ee7b7
omithassain28 <115416573+omithassain28@users.noreply.github.com>
2024-03-01 13:05:06 +0600

    Update PlayerBase.js

--- src/structures/PlayerBase.js
+++ src/structures/PlayerBase.js
@@ -1,17 +1,17 @@
-const { Message } = require("discord.js");
-const { Structure } = require("erela.js");
+const { MessageEmbed } = require("discord.js");
+const { Manager } = require("erela.js");
 
-// This system from discord music bot https://github.com/SudhanPlayz
+// Updated system from a Discord music bot by SudhanPlayz
 
-Structure.extend(
+Manager.extend(
   "Player",
   (Player) =>
     class extends Player {
       /**
-       * Sets now playing message for deleting next time
-       * @param {Message} message
+       * Sets the currently playing message for future deletion
+       * @param {MessageEmbed} message
        */
-      setNowplayingMessage(message) {
+      setNowPlayingMessage(message) {
         if (this.nowPlayingMessage && !this.nowPlayingMessage.deleted)
           this.nowPlayingMessage.delete();
         return (this.nowPlayingMessage = message);
