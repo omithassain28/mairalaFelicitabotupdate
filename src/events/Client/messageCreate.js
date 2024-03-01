e11a97a497e801e9757144608cbfdf8c13e9b1bf
omithassain28 <115416573+omithassain28@users.noreply.github.com>
2024-03-01 13:30:46 +0600

    Update messageCreate.js

--- src/events/Client/messageCreate.js
+++ src/events/Client/messageCreate.js
@@ -1,15 +1,8 @@
-// Import necessary modules
-const { Client, Message, MessageEmbed } = require("discord.js");
-const { Permissions } = require("discord.js");
-
-// Import required modules from schema and utils
+const { Client, MessageEmbed, Permissions } = require("discord.js");
 const db = require("../../schema/prefix.js");
 const i18n = require("../../utils/i18n");
-
-// Define the owner
 const owner = ["823101214564417536"];
 
-// Define the function for message creation
 module.exports = {
   name: "messageCreate",
   run: async (client, message) => {
@@ -17,7 +10,7 @@ module.exports = {
     if (!message.guild) return;
     let prefix = client.prefix;
 
-    const channel = message.channel;
+    const channel = message?.channel;
     const ress = await db.findOne({ Guild: message.guildId });
     if (ress && ress.Prefix) prefix = ress.Prefix;
 
@@ -52,93 +45,80 @@ module.exports = {
       np = [];
     }
     let regex = new RegExp(`^<@!?${client.user.id}>`);
-    let pre = message.content.match(regex)
-      ? message.content.match(regex)[0]
-      : prefix;
+    let pre = message.content.match(regex) ? message.content.match(regex)[0] : prefix;
     if (!np.includes(message.author.id)) {
       if (!message.content.startsWith(pre)) return;
     }
 
     const args =
-      np.includes(message.author.id) === false
-        ? message.content.slice(pre.length).trim().split(/ +/)
-        : message.content.startsWith(pre) === true
-          ? message.content.slice(pre.length).trim().split(/ +/)
-          : message.content.trim().split(/ +/);
+      np.includes(message.author.id) === false ? message.content.slice(pre.length).trim().split(/ +/) :
+        message.content.startsWith(pre) === true ? message.content.slice(pre.length).trim().split(/ +/) :
+          message.content.trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     const command =
       client.commands.get(commandName) ||
-      client.commands.find(
-        (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-      );
+      client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
     if (!command) return;
+
     if (command.voteonly) {
       let vote = await client.topgg.hasVoted(message.author.id);
       if (!vote) {
         let embed = new MessageEmbed()
-          .setDescription(
-            `<:online:1210253399031812147> | **This Is A Vote Required Command So Vote Us Now By [Clicking Here](https://top.gg/bot/${client.user.id}/vote)**`
-          )
+          .setDescription(`<:online:1210253399031812147> | **This Is A Vote Required Command So Vote Us Now By [Clicking Here](https://top.gg/bot/${client.user.id}/vote)**`)
           .setColor(client.embedColor);
         return message.channel.send({ embeds: [embed] });
       }
     }
-    if (!message.guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES))
-      return await message.author.dmChannel
-        .send({
-          content: `${i18n.__mf("events.msgcrt.embed2", {
-            channelId: message.channel.id,
-            cmdname: command.name
-          })}`
-        }).catch(() => {});
 
-    if (!message.guild.me.permissions.has(Permissions.FLAGS.VIEW_CHANNEL))
+    if (!message.guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES)) {
+      await message.author.dmChannel.send({
+        content: `${i18n.__mf("events.msgcrt.embed2", { channelId: message.channel.id, cmdname: command.name })}`
+      }).catch(() => {});
+
       return;
+    }
 
-    if (!message.guild.me.permissions.has(Permissions.FLAGS.EMBED_LINKS))
-      return await message.channel
-        .send({
-          content: `${i18n.__mf("events.msgcrt.embed3", {
-            cmdname: command.name
-          })}`
-        })
-        .catch(() => {});
+    if (!message.guild.me.permissions.has(Permissions.FLAGS.VIEW_CHANNEL)) return;
+
+    if (!message.guild.me.permissions.has(Permissions.FLAGS.EMBED_LINKS)) {
+      await message.channel.send({
+        content: `${i18n.__mf("events.msgcrt.embed3", { cmdname: command.name })}`
+      }).catch(() => {});
+
+      return;
+    }
 
     const embed = new MessageEmbed().setColor("2f3136");
 
     if (command.args && !args.length) {
       let reply = `${i18n.__("events.msgcrt.embed4")}`;
+
       if (command.usage) {
         reply += `\n\`${prefix}${command.name} ${command.usage}\``;
       }
+
       embed.setDescription(reply);
       return message.channel.send({ embeds: [embed] });
     }
 
-    if (
-      command.permission &&
-      !message.member.permissions.has(command.permission)
-    ) {
+    if (command.permission && !message.member.permissions.has(command.permission)) {
       embed.setDescription(i18n.__("prams.prams"));
       return message.channel.send({ embeds: [embed] });
     }
-    if (
-      !channel.permissionsFor(message.guild.me)
-        ?.has(Permissions.FLAGS.EMBED_LINKS) &&
-      client.user.id !== userId
-    ) {
+
+    if (!channel.permissionsFor(message.guild.me)?.has(Permissions.FLAGS.EMBED_LINKS) && client.user.id !== message.author.id) {
       return channel.send({ content: `${i18n.__("events.msgcrt.embed5")}` });
     }
+
     if (command.owner) {
       if (!owner.includes(message.author.id)) {
-        embed.setDescription(
-          "<:online:1210253399031812147> | Only My Developers Can Use This Command"
-        );
+        embed.setDescription("<:online:1210253399031812147> | Only My Developers Can Use This Command");
         return message.channel.send({ embeds: [embed] });
       }
     }
+
     const player = message.client.manager.get(message.guild.id);
 
     if (command.player && !player) {
@@ -153,12 +133,8 @@ module.exports = {
 
     if (command.sameVoiceChannel) {
       if (message.guild.me.voice.channel) {
-        if (
-          message.guild.me.voice.channelId !== message.member.voice.channelId
-        ) {
-          embed.setDescription(
-            `${i18n.__("player.samevc")} ${message.guild.me.voice.channel}!`
-          );
+        if (message.guild.me.voice.channelId !== message.member.voice.channelId) {
+          embed.setDescription(`${i18n.__("player.samevc")} ${message.guild.me.voice.channel}!`);
           return message.channel.send({ embeds: [embed] });
         }
       }
@@ -171,5 +147,5 @@ module.exports = {
       embed.setDescription(i18n.__("player.cmderr"));
       return message.channel.send({ embeds: [embed] });
     }
-  }
+  },
 };
